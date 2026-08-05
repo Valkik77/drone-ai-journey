f72570f28d52b30dc6ffd725a377e24f61857350
Bo-cheng <willandark17@gmail.com>
2026-08-05 13:07:07 +0800

    超簡單程式小練習:奇偶判斷/迴圈/最近點計算/函式封裝

--- practice1.py
+++ practice1.py
@@ -1,4 +1,36 @@
+number = int(input("請輸入一個數字:"))
+if number % 2 == 0:
+    print("這個數字是偶數")
+else:
+    print("這個數字是奇數")
+
 i=0
 for i in range(1,100):
     if i%3==0:
         print(i,end=" ")
+
+print("\n")
+points = [(3, 4), (1, 1), (10, 10), (0, 5)]
+point=points[0]
+dis=points[0][0] ** 2 + points[0][1] ** 2
+for j in range(1, len(points)):
+    p=points[j]
+    x = (points[j][0] - points[j-1][0]) ** 2 + (points[j][1] - points[j-1][1]) ** 2
+    if x< dis:
+        point=points[j]
+print("距离原点最近的点是:",point)
+
+
+def find_closest_point(points):
+    closest_point = points[0]
+    min_distance = points[0][0]**2+points[0][1]**2
+    for point in points[1:]:
+        distance = point[0]**2 + point[1]**2
+        if distance < min_distance:
+            closest_point = point
+            min_distance = distance
+    return closest_point
+
+points = [(2, 4), (3, 3), (10, 0), (1, 5)]
+closest_point = find_closest_point(points)
+print("距离原点最近的点是:", closest_point)
